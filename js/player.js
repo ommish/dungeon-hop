1068647d5e8e99a36b5c2b06163d7d141b5b1a14
Ommi Shimizu <oshimizu15@gmail.com>
2017-11-22 08:48:43 -0500

    Change player shape and add player color

--- js/player.js
+++ js/player.js
@@ -1,18 +1,27 @@
 const Timer = require('../node_modules/easytimer.js/dist/easytimer.min.js');
 
+const _colors = ["red", "blue"];
+
 class HumanPlayer {
   constructor(i, ctx) {
     this.playerNumber = i;
     this.ctx = ctx;
     this.timer = new Timer();
     this.timer.start({precision: 'secondTenths'});
+    this.x = 0;
+    this.y = 0;
+    this.radius = 30;
+    this.color = _colors[i - 1];
   }
 
   drawPlayer() {
+
     this.ctx.beginPath();
-    this.ctx.lineWidth="6";
-    this.ctx.strokeStyle="red";
-    this.ctx.rect(100,100,300,300);
+    this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+    this.ctx.lineWidth = 5;
+    this.ctx.strokeStyle = '#003300';
     this.ctx.stroke();
   }
 
@@ -29,6 +38,7 @@ class HumanPlayer {
 
   jump(spaces) {
     console.log(`I'm jumping ${spaces} spaces!`);
+
   }
 }
 

c26b6b929c9c94915a857707318cd5b7e7ba1745
Ommi Shimizu <oshimizu15@gmail.com>
2017-12-01 09:58:09 -0500

    Move firebase auth before dom load

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 const Game = require('./game.js');
 
-document.addEventListener("DOMContentLoaded", () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const token = result.credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    const user = result.user;
-  }).catch(function(error) {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    const credential = error.credential;
-    // ...
-  });
+const provider = new firebase.auth.GoogleAuthProvider();
 
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  const user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  // The email of the user's account used.
+  const email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  const credential = error.credential;
+  // ...
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
   const canvasEl = document.getElementsByTagName('canvas')[0];
   new Game(canvasEl, user);
 });

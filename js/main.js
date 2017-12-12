729e18b1f728b1feb489f65a0992bb1db7ff7a9d
Ommi Shimizu <oshimizu15@gmail.com>
2017-12-12 11:56:40 -0500

    Move auth to main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
 const Game = require('./game.js');
 const SettingsForm = require('./settings_form.js');
 const StartMenu = require('./start_menu.js');
-const Auth = require('./auth.js');
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      const displayName = user.displayName;
+      const email = user.email;
+      const emailVerified = user.emailVerified;
+      const uid = user.uid;
+      const providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const token = result.credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    const credential = error.credential;
+    // ...
+  });
+
   const canvasEl = document.getElementsByTagName('canvas')[0];
   const musicButton = $(".music-button");
   const settingsSubmitButton = document.getElementById("submit-settings");
